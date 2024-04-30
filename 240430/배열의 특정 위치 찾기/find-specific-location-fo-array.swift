fc5a83c0b1f39b82fdf4a3f1e9027aa575afa1ba
HM <135616789+dxdlar@users.noreply.github.com>
2024-04-30 10:28:13 +0900

    Title: 배열의 특정 위치 찾기, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240430/배열의 특정 위치 찾기/find-specific-location-fo-array.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map{Int($0)!}
+var sum1 = 0
+var sum2 = 0
+var cnt = 0
+var avr = 0
+
+for i in stride(from: 1, through: 9, by: 2) {
+    sum1 += arr[i]
+}
+
+for j in stride(from: 2, through: 9, by: 3) {
+    sum2 += arr[j]
+    cnt += 1
+}
+
+print(sum1, (Double(sum2) / Double(cnt) * 10) / 10)
