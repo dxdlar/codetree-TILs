b67dc1a6a94488cb3ce47a3a4f1a4e1f929348e2
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-07 16:16:24 +0900

    Title: 배열 놀이, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240507/배열 놀이/play-with-array.swift
+++ 240507/배열 놀이/play-with-array.swift
@@ -6,25 +6,28 @@ let (n, q) = {
 }()
 
 let comp = readLine()!.split(separator: " ").map{Int($0)!} // 원소 n의 배열
-var comp_index = 0
 
-for i in 0..<q {
+
+for i in 1...q {
+    var comp_index = 0
     var arr = readLine()!.split(separator: " ").map{Int($0)!} // 질의 q의 배열
     if arr[0] == 1 {
         print(comp[arr[1] - 1])
     } else if arr[0] == 2 {
         for j in 0..<n {
-            if comp[j] != arr[1] {
-                comp_index += 1
-            } else {
+            if comp[j] == arr[1] {
                 comp_index += 1
                 break
+            } else {
+                comp_index += 1
             }
         }
+        if comp_index == n {
+            comp_index = 0
+        }
         print(comp_index)
-        comp_index = 0
     } else if arr[0] == 3 {
-        for k in arr[1]-1...arr[2]-1 {
+        for k in arr[1]-1..<arr[2] {
             print(comp[k], terminator: " ")
         }
         print()
