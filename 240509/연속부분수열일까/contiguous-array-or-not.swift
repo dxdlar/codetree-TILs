e5c34250baa57427b6afda5100588178d559dc04
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-09 10:09:01 +0900

    Title: 연속부분수열일까, Time: 20ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 240509/연속부분수열일까/contiguous-array-or-not.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let (n1, n2) = {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    return (input[0], input[1])
+}()
+
+let arr1 = readLine()!.split(separator: " ").map{Int($0)!}
+let arr2 = readLine()!.split(separator: " ").map{Int($0)!}
+var cnt = true
+var new_arr: [Int] = []
+
+for i in 0...n1-n2 {
+    cnt = true
+    for j in 0..<n2 {
+        if arr1[i+j] != arr2[j] {
+            cnt = false
+            break
+        }
+    }
+    if cnt == true {
+        break
+    }
+}
+
+if cnt == true {
+    print("Yes")
+} else {
+    print("No")
+}
