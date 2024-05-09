df85cc299e2bdb41398897ce74dbf92654e54a42
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-09 10:14:52 +0900

    Title: 연속부분수열일까, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

--- 240509/연속부분수열일까/contiguous-array-or-not.swift
+++ 240509/연속부분수열일까/contiguous-array-or-not.swift
@@ -7,19 +7,21 @@ let (n1, n2) = {
 
 let arr1 = readLine()!.split(separator: " ").map{Int($0)!}
 let arr2 = readLine()!.split(separator: " ").map{Int($0)!}
-var cnt = true
+var cnt = false
 var new_arr: [Int] = []
 
-for i in 0...n1-n2 {
-    cnt = true
-    for j in 0..<n2 {
-        if arr1[i+j] != arr2[j] {
-            cnt = false
+if n1 >= n2 {
+    for i in 0...n1-n2 {
+        cnt = true
+        for j in 0..<n2 {
+            if arr1[i+j] != arr2[j] {
+                cnt = false
+                break
+            }
+        }
+        if cnt == true {
             break
         }
-    }
-    if cnt == true {
-        break
     }
 }
 
