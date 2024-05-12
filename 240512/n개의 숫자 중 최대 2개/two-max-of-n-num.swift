7814feb2cc2c8dadd9f0ee78c50200b46fd0ea48
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-12 11:13:38 +0900

    Title: n개의 숫자 중 최대 2개, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240512/n개의 숫자 중 최대 2개/two-max-of-n-num.swift
+++ 240512/n개의 숫자 중 최대 2개/two-max-of-n-num.swift
@@ -2,24 +2,18 @@ import Foundation
 
 let N = Int(readLine()!)!
 let arr = readLine()!.split(separator: " ").map{Int($0)!}
-var new_arr: [Int] = [0, 0]
 
-new_arr[0] = arr[0]
-new_arr[1] = arr[0]
+var max1 = 0
+var max2 = 0
 
-// 첫 번째로 큰 원소
-for i in 0..<N {
-    if new_arr[0] <= arr[i] {
-        new_arr[0] = arr[i]
+for i in arr {
+    if i > max2 {
+        max2 = i
+        if i > max1 {
+            max2 = max1
+            max1 = i
+        }
     }
 }
 
-// 두 번째로 큰 원소
-// (첫 번쨰 원소 구할 떄와 같지만 첫 번쨰 원소보다 작거나 같아야 함)
-for j in 0..<N {
-    if new_arr[1] <= arr[j] && arr[j] < new_arr[0] {
-        new_arr[1] = arr[j]
-    }
-}
-
-print(new_arr[0], new_arr[1])
+print(max1, max2)
