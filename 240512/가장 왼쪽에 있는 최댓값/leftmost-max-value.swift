f067e80c48b8c7349a0ec6ea4c870110fd219052
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-12 15:57:53 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 19ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 240512/가장 왼쪽에 있는 최댓값/leftmost-max-value.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int($0)!}
+var cnt_arr = arr
+var new_arr: [Int] = []
+var cnt = 0
+
+// 순서 : 최댓값 찾기 -> cnt 구하기, 저장 -> cnt 까지 배열 자르기 -> 반복
+// 주의점 : arr.max 사용, arr가 계속 변해야 함, while로 반복, max 값으로 new_arr에 append
+
+while true {
+    for i in cnt_arr {
+        cnt += 1
+        if i == cnt_arr.max()! {
+            new_arr.append(cnt)
+            break
+        }
+    }
+    if cnt == 1 {
+        break
+    }
+    cnt_arr = []
+    for j in new_arr {
+        cnt_arr.append(arr[j])
+    }
+    cnt = 0
+}
+
+for j in 0..<new_arr.count {
+    print(new_arr[j], terminator: " ")
+}
