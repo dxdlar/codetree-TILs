579a7b17e1ef584b65672913d6651bd31096fc09
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-23 10:58:30 +0900

    Title: 세 실수의 반올림, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240523/세 실수의 반올림/rounding-of-three-actual-numbers.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let a = Double(readLine()!)!
+let b = Double(readLine()!)!
+let c = Double(readLine()!)!
+
+print(String(format: "%.3f", a))
+print(String(format: "%.3f", b))
+print(String(format: "%.3f", c))
