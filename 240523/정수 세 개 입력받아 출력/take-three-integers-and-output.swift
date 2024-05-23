8820bb5ed6837ce98bda747e34afc3b5ad2e9198
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-23 11:13:55 +0900

    Title: 정수 세 개 입력받아 출력, Time: 17ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240523/정수 세 개 입력받아 출력/take-three-integers-and-output.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let (a, b) = {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    return (input[0], input[1])
+}()
+
+let c = Int(readLine()!)!
+
+print(a, b, c)
