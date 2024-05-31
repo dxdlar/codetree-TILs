a0d8425b869cea65619a7bee12179383ab1e069a
HM <135616789+dxdlar@users.noreply.github.com>
2024-05-31 13:41:04 +0900

    Title: 그 전 알파벳, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240531/그 전 알파벳/before-the-alphabet.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+var n = Int(UnicodeScalar(readLine()!)!.value)
+
+// a = 97
+// z = 122
+
+if n == 97 {
+    n = 122
+    print(String(UnicodeScalar(n)!))
+} else {
+    n -= 1
+    print(String(UnicodeScalar(n)!))
+}
